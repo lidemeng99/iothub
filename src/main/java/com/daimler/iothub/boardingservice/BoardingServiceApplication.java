fd31bcbd2a29438e50bf60721fbccff9e3ff065f
lidemeng <lidemeng99@icloud.com>
2018-11-22 09:59:27 +0800

    Initial commit

---
+++ src/main/java/com/daimler/iothub/boardingservice/BoardingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.daimler.iothub.boardingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardingServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/daimler/iothub/boardingservice/handler/HandleService.java
@@ -0,0 +1,19 @@
+package com.daimler.iothub.boardingservice.handler;
+
+import com.rabbitmq.client.Channel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HandleService implements ChannelAwareMessageListener {
+    private static final Logger logger = LoggerFactory.getLogger(HandleService.class);
+    @Override
+    public void onMessage(Message message, Channel channel) throws Exception {
+      
+       logger.info( message.getMessageProperties().getConsumerQueue()+":"+new String(message.getBody()));
+
+    }
+}
